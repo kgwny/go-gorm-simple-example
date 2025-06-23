32866ee1ba57d2e6c1c23df212d53df35472c1c7
kgwny <210300265+kgwny@users.noreply.github.com>
2025-06-24 01:08:52 +0900

    update main.go

--- main.go
+++ main.go
@@ -2,34 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 )
 
 func main() {
-	// .envファイルの取得
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf(".env が読み込めませんでした。: %v", err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
 
-	// 環境変数の取得
-	m1 := os.Getenv("SAMPLE_MESSAGE")
-	fmt.Println(m1)
-
-	// 環境変数に追加して取得
-	os.Setenv("ADD_MESSAGE", "HOGE")
-	m2 := os.Getenv("ADD_MESSAGE")
-	fmt.Println(m2)
-
-	// 環境変数の削除
-	os.Unsetenv("SAMPLE_MESSAGE")
-	m3 := os.Getenv("SAMPLE_MESSAGE")
-	fmt.Println(m3)
-
-	// すべての環境変数を削除
-	os.Clearenv()
-	m4 := os.Getenv("ADD_MESSAGE")
-	fmt.Println(m4)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
