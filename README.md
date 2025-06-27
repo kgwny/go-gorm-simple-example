77c223a8fc86a6ff1631b2c2fa4e6667ce964338
kgwny <210300265+kgwny@users.noreply.github.com>
2025-06-28 00:32:52 +0900

    update

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # go-gorm-simple-example
 
-### about air
-air は Docker を用いて Go による開発を行う際、ホットリロードしてくれるツール
-
 ### dockerコンテナ起動
 ```
 docker compose up
-```
-
-### dockerコンテナのシェルにアクセス
-```
-docker-compose exec -it api sh
 ```
--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	dsn := "root@tcp(mysql:3306)/first?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	var product Product
+	db.First(&product, "code = ?", "D42") // find product with code D42
+	fmt.Println(product)
 }
